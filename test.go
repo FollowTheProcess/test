4f33cc3958636dff75788eae0464d0b5461018bb
Tom Fleet <tomfleet2018@gmail.com>
2023-06-04 17:12:58 +0100

    Initial Equal and EqualFunc implementations

--- test.go
+++ test.go
@@ -1,7 +1,25 @@
-// Package test is a placeholder for something cool.
+// Package test provides a lightweight, but useful extension to the std lib's testing package
+// for a friendlier and more intuitive API, as well some enhanced functionality like rich
+// comparison and colour output.
 package test
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello test"
+import "testing"
+
+// Equal fails if got != want.
+func Equal[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Fatalf("\nGot:\t%+v\nWanted:\t%+v\n", got, want)
+	}
+}
+
+// EqualFunc is like Equal but allows the user to pass a custom comparator, useful
+// when the items to be compared to not implement the comparable generic constraint
+//
+// The comparator should return true if got and want should be considered equal.
+func EqualFunc[T any](t testing.TB, got, want T, comparator func(got, want T) bool) {
+	t.Helper()
+	if !comparator(got, want) {
+		t.Fatalf("\nGot:\t%+v\nWanted:\t%+v\n", got, want)
+	}
 }
--- test_test.go
+++ test_test.go
@@ -1,16 +1,46 @@
 package test_test
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"testing"
 
 	"github.com/FollowTheProcess/test"
 )
 
-func TestHello(t *testing.T) {
-	got := test.Hello()
-	want := "Hello test"
+type TB struct {
+	testing.TB
+	out    io.Writer
+	failed bool
+}
 
-	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
+func (t *TB) Helper() {}
+
+func (t *TB) Fatalf(format string, args ...any) {
+	t.failed = true
+	fmt.Fprintf(t.out, format, args...)
+}
+
+func TestTest(t *testing.T) {
+	shouldPass := func(fn func(tb testing.TB)) {
+		t.Helper()
+		buf := &bytes.Buffer{}
+		tb := &TB{out: buf}
+
+		// Call our test function
+		fn(tb)
+
+		if tb.failed {
+			t.Fatal("Initial failed state should be false")
+		}
+
+		if buf.String() != "" {
+			t.Fatalf("Shouldn't have written anything on success\nGot:\t%+v\n", buf.String())
+		}
 	}
+	shouldPass(func(tb testing.TB) { test.Equal(tb, "hello", "hello") })
+	shouldPass(func(tb testing.TB) { test.Equal(tb, 42, 42) })
+	shouldPass(func(tb testing.TB) { test.Equal(tb, true, true) })
+	shouldPass(func(tb testing.TB) { test.Equal(tb, 3.14, 3.14) })
 }

9a4e1dbfecbe0b4e0a6024eb975329cac996a47a
Tom Fleet <tomfleet2018@gmail.com>
2023-06-05 08:31:07 +0100

    Add NotEqual and NotEqualFunc

--- test.go
+++ test.go
@@ -14,12 +14,31 @@ func Equal[T comparable](t testing.TB, got, want T) {
 }
 
 // EqualFunc is like Equal but allows the user to pass a custom comparator, useful
-// when the items to be compared to not implement the comparable generic constraint
+// when the items to be compared do not implement the comparable generic constraint
 //
 // The comparator should return true if got and want should be considered equal.
 func EqualFunc[T any](t testing.TB, got, want T, comparator func(got, want T) bool) {
 	t.Helper()
 	if !comparator(got, want) {
 		t.Fatalf("\nGot:\t%+v\nWanted:\t%+v\n", got, want)
+	}
+}
+
+// NotEqual fails if got == want.
+func NotEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Fatalf("\nValues were equal:\t%+v\n", got)
+	}
+}
+
+// NotEqualFunc is like NotEqual but allows the user to pass a custom comparator, useful
+// when the items to be compared do not implement the comparable generic constraint
+//
+// The comparator should return true if got and want should be considered equal.
+func NotEqualFunc[T any](t testing.TB, got, want T, comparator func(got, want T) bool) {
+	t.Helper()
+	if comparator(got, want) {
+		t.Fatalf("\nValues were equal:\t%+v\n", got)
 	}
 }
--- test_test.go
+++ test_test.go
@@ -49,12 +49,33 @@ func TestPass(t *testing.T) {
 	shouldPass(func(tb testing.TB) { test.Equal(tb, true, true) })
 	shouldPass(func(tb testing.TB) { test.Equal(tb, 3.14, 3.14) })
 
+	shouldPass(func(tb testing.TB) { test.NotEqual(tb, "hello", "there") })
+	shouldPass(func(tb testing.TB) { test.NotEqual(tb, 42, 27) })
+	shouldPass(func(tb testing.TB) { test.NotEqual(tb, true, false) })
+	shouldPass(func(tb testing.TB) { test.NotEqual(tb, 3.14, 8.67) })
+
 	shouldPass(func(tb testing.TB) {
 		test.EqualFunc(tb, "something", "equal", func(got, want string) bool { return true })
 	})
 
 	shouldPass(func(tb testing.TB) {
 		test.EqualFunc(tb, 42, 42, func(got, want int) bool { return true })
+	})
+
+	shouldPass(func(tb testing.TB) {
+		test.EqualFunc(tb, []string{"hello"}, []string{"hello"}, func(got, want []string) bool { return true })
+	})
+
+	shouldPass(func(tb testing.TB) {
+		test.NotEqualFunc(tb, "something", "different", func(got, want string) bool { return false })
+	})
+
+	shouldPass(func(tb testing.TB) {
+		test.NotEqualFunc(tb, 42, 12, func(got, want int) bool { return false })
+	})
+
+	shouldPass(func(tb testing.TB) {
+		test.NotEqualFunc(tb, []string{"hello"}, []string{"something", "else"}, func(got, want []string) bool { return false })
 	})
 }
 
@@ -85,11 +106,32 @@ func TestFail(t *testing.T) {
 	shouldFail(func(tb testing.TB) { test.Equal(tb, true, false) })
 	shouldFail(func(tb testing.TB) { test.Equal(tb, 3.14, 8.96) })
 
+	shouldFail(func(tb testing.TB) { test.NotEqual(tb, "something", "something") })
+	shouldFail(func(tb testing.TB) { test.NotEqual(tb, 42, 42) })
+	shouldFail(func(tb testing.TB) { test.NotEqual(tb, true, true) })
+	shouldFail(func(tb testing.TB) { test.NotEqual(tb, 3.14, 3.14) })
+
 	shouldFail(func(tb testing.TB) {
 		test.EqualFunc(tb, "something", "different", func(got, want string) bool { return false })
 	})
 
 	shouldFail(func(tb testing.TB) {
 		test.EqualFunc(tb, 42, 127, func(got, want int) bool { return false })
+	})
+
+	shouldFail(func(tb testing.TB) {
+		test.EqualFunc(tb, []int{42}, []int{27}, func(got, want []int) bool { return false })
+	})
+
+	shouldFail(func(tb testing.TB) {
+		test.NotEqualFunc(tb, "something", "something", func(got, want string) bool { return true })
+	})
+
+	shouldFail(func(tb testing.TB) {
+		test.NotEqualFunc(tb, 42, 42, func(got, want int) bool { return true })
+	})
+
+	shouldFail(func(tb testing.TB) {
+		test.NotEqualFunc(tb, []int{42}, []int{42}, func(got, want []int) bool { return true })
 	})
 }
